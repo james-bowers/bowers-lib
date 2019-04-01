914a653e5b0e88e0f5dd41eeae24e4736b612c4d
James Bowers <james@james-bowers.me>
2019-04-01 11:37:26 +0100

    run mix new

---
+++ lib/ex_bowers.ex
@@ -0,0 +1,18 @@
+defmodule ExBowers do
+  @moduledoc """
+  Documentation for ExBowers.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBowers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_bowers_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBowersTest do
+  use ExUnit.Case
+  doctest ExBowers
+
+  test "greets the world" do
+    assert ExBowers.hello() == :world
+  end
+end
