5c72adb5971432eb4402de5de91c1aec87b647b8
James Bowers <james@james-bowers.me>
2019-04-02 16:41:16 +0100

    View module and struct to tightly define view contract

---
+++ lib/ex_bowers/view/view.ex
@@ -0,0 +1,20 @@
+defmodule ExBowers.View do    
+  import Plug.Conn
+
+  @enforce_keys [:description, :content]
+  defstruct [:description, :content]
+
+  def send_json(conn, status, %__MODULE__{} = content) do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(status, Poison.encode!(content))
+  end
+
+  def format_changeset(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
+end
